b7db08f4cb9dbbf32b0070e26636eb9812b30571
mposolda <mposolda@gmail.com>
2024-12-12 18:31:03 +0100

    Renaming files back
    Signed-off-by: mposolda <mposolda@gmail.com>

--- run-tests.sh
+++ run-tests.sh
@@ -32,15 +32,14 @@ run_tests() {
   fi
 }
 
-#if [[ "$1" == "group1" ]]; then
-#  for i in `ls test/*.js | grep -v "enforcer-spec"`; do run_tests ${i}; done
-#fi
-#
-#if [[ "$1" == "group2" ]]; then
-#  for i in `ls test/*.js | grep "enforcer-spec"`; do run_tests ${i}; done
-#fi
-#
-#if [[ -z "$1" ]]; then
-#  for i in `ls test/*.js`; do run_tests ${i}; done
-#fi
-run_tests test/keycloak-connect-web-enforcer-spec.js
+if [[ "$1" == "group1" ]]; then
+  for i in `ls test/*.js | grep -v "enforcer-spec"`; do run_tests ${i}; done
+fi
+
+if [[ "$1" == "group2" ]]; then
+  for i in `ls test/*.js | grep "enforcer-spec"`; do run_tests ${i}; done
+fi
+
+if [[ -z "$1" ]]; then
+  for i in `ls test/*.js`; do run_tests ${i}; done
+fi
